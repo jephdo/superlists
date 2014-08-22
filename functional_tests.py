158651d9677a505aeea16474e0fefb5786e16142
Jeph Do <jdo@solarcity.com>
2014-08-22 04:29:32 -0700

    first FT specced out and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # a new user visits the webpage
+        self.browser.get('http://localhost:8000')
+
+        # she notices the page t itle and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        #
+
+if __name__ == '__main__':
+    unittest.main()
